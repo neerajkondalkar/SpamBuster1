f76b4878b52e13bcd839054f31e678a3e9be742d
sweetpoision <42069468+NeerajKondalkar@users.noreply.github.com>
2020-07-13 21:29:24 +0530

    Update ComposeSmsActivity.java

--- app/src/main/java/com/example/mynewsmsapp_kotlin/ComposeSmsActivity.java
+++ app/src/main/java/com/example/mynewsmsapp_kotlin/ComposeSmsActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +17,8 @@ import androidx.core.content.ContextCompat;
 public class ComposeSmsActivity extends AppCompatActivity {
 
     private static final String TAG = "[MY_DEBUG] " + ComposeSmsActivity.class.getSimpleName();
-
+    EditText input_contact;
+    EditText input_sms;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         String TAG_onCreate = " onCreate(): ";
@@ -24,8 +26,8 @@ public class ComposeSmsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_sms);
 
-        EditText input_contact = findViewById(R.id.edit_text_phone_or_name);
-        EditText input_sms = findViewById(R.id.sms_text_input);
+        input_contact = (EditText) findViewById(R.id.edit_text_phone_or_name);
+        input_sms = (EditText) findViewById(R.id.sms_text_input);
 
         checkNecessaryPermissions();
 //        Button button_send_sms = findViewById(R.id.button_send_sms);
@@ -35,6 +37,7 @@ public class ComposeSmsActivity extends AppCompatActivity {
 
     public void checkNecessaryPermissions() {
         String TAG_checkNecessaryPermissions = " checkNecessaryPermissions(): ";
+        Log.d(TAG, TAG_checkNecessaryPermissions + " called");
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, TAG_checkNecessaryPermissions + " SEND_SMS permission granted");
         } else {
@@ -45,6 +48,30 @@ public class ComposeSmsActivity extends AppCompatActivity {
     public void onClickSendSms(View view){
         String TAG_onClickSendSms = " onClickSendSms(): ";
         Log.d(TAG, TAG_onClickSendSms + " called");
+        input_contact  = findViewById(R.id.edit_text_phone_or_name);
+        input_sms = findViewById(R.id.edit_text_input_message);
+
+        try {
+            String str_input_contact = input_contact.getText().toString();
+            String str_input_sms = input_sms.getText().toString();
+
+            if(str_input_contact.equals("") || str_input_sms.equals("")) {
+                Toast.makeText(this, "Empty phone/message!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Input accepted", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, TAG_onClickSendSms + "str_input_contact = " + str_input_contact);
+                Log.d(TAG, TAG_onClickSendSms + "str_input_sms  = " + str_input_sms);
+            }
+//        if(input_contact.toString() !> ) {
+//        }
+        }
+        //null pointer exception sometimes
+        catch (Exception e){
+            Log.d(TAG, TAG_onClickSendSms + "Exception : " + e);
+            Toast.makeText(this, "Empty phone/message!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public void onClickBackToMainActivity(View view){
