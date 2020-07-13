065e1017b36cfed3b23c255f9197db59d0e40c97
sweetpoision <42069468+NeerajKondalkar@users.noreply.github.com>
2020-07-13 19:46:53 +0530

    compose sms under construction

--- app/src/main/java/com/example/mynewsmsapp_kotlin/ComposeSmsActivity.java
+++ app/src/main/java/com/example/mynewsmsapp_kotlin/ComposeSmsActivity.java
@@ -1,6 +1,8 @@
 package com.example.mynewsmsapp_kotlin;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class ComposeSmsActivity extends AppCompatActivity {
 
@@ -21,12 +24,22 @@ public class ComposeSmsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_sms);
 
-        EditText input_phone_number = findViewById(R.id.edit_text_phone_number);
+        EditText input_contact = findViewById(R.id.edit_text_phone_or_name);
         EditText input_sms = findViewById(R.id.sms_text_input);
 
+        checkNecessaryPermissions();
 //        Button button_send_sms = findViewById(R.id.button_send_sms);
 //        Button button_back = findViewById(R.id.button_back_to_activity_main);
 
+    }
+
+    public void checkNecessaryPermissions() {
+        String TAG_checkNecessaryPermissions = " checkNecessaryPermissions(): ";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, TAG_checkNecessaryPermissions + " SEND_SMS permission granted");
+        } else {
+            Log.d(TAG, TAG_checkNecessaryPermissions + " SEND_SMS permission denied");
+        }
     }
 
     public void onClickSendSms(View view){
